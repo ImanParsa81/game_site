6aa8fb535593558e1eb6cff2b34da1196d30f8d5
ImanParsa81 <165454218+ImanParsa81@users.noreply.github.com>
2025-11-21 20:37:26 -0800

    1.0.0.2

--- footer.php
+++ footer.php
@@ -2,13 +2,15 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-12 col-md-6 text-center text-md-left mb-3 mb-md-0">
-                <p class="pars_footer_copyright">© 2024 GameLearn. All rights reserved.</p>
+                <p class="pars_footer_copyright"> تمامی حقوق این سایت متعلق به ویرا آموزگار ایساتیس است . </p>
             </div>
             <div class="col-12 col-md-6 text-center text-md-right">
                 <nav class="pars_footer_nav">
-                    <a href="#" class="pars_footer_link">Privacy Policy</a>
-                    <a href="#" class="pars_footer_link">Terms of Service</a>
-                    <a href="#" class="pars_footer_link">Contact Us</a>
+
+                        <a href="#" class="pars_footer_link">حریم خصوصی</a>
+                        <a href="#" class="pars_footer_link">شرایط استفاده</a>
+                        <a href="#" class="pars_footer_link">تماس با ما</a>
+               
                 </nav>
             </div>
         </div>
